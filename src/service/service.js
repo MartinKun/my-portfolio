1fc5b1f5e24bd3613be62f622d2abd2f91a4b6da
Martin Kun <tinbrc2010k@hotmail.com>
2022-07-29 20:55:35 -0300

    changing baseUrl

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-/*const baseUrl = 'https://works-app.herokuapp.com';*/
-const baseUrl = 'http://localhost:8080';
+const baseUrl = 'https://works-app.herokuapp.com';
 
 export const getFeaturedWorks = (index) => {
     return axios({
