70ee4e270ed482d9b889f4b1987f0ec757a7eb65
MartinKun <martinkunbrc@hotmail.com>
2022-12-29 22:03:05 -0300

    new update

--- src/sections/Projects/index.js
+++ src/sections/Projects/index.js
@@ -14,94 +14,44 @@ const Projects = () => {
   const { reveal } = useContext(ScrollRevealContext);
   const { isLightOn } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
-  const [works, setWorks] = useState({ array: [], page: 0, limitItems: 0 });
-  const [isShowMore, setIsShowMore] = useState(false);
-  const [callbacks, setCallBacks] = useState({
-    error: false,
-    loadingFirstWorks: false,
-    loadingMoreWorks: false,
+  const [works, setWorks] = useState({
+    array: [
+      {
+        idWork: 1,
+        titleEn: "Parking Management System",
+        titleEs: "Sistema de Gestión de Parquímetro",
+        descriptionEn: "Desktop application for the management of a parking system, realized with Java. The employee would be able to carry out an appropriate register of the vehicles parked, charge the clients the precise amount of parking time used, and do proper cash control.",
+        descriptionEs: "Aplicación de escritorio para la administración de un estacionamiento realizado con Java. El empleado podrá llevar a cabo un registro adecuado de los vehículos aparcados, cobrar por tiempo de utilización y hacer un correcto control de caja.",
+        image: "https://i.im.ge/2022/07/21/Fdwt9M.jpg",
+        techs: ["Java SE 8", "Swing", "SQLite", "Eclipse Photon"],
+        github: "https://github.com/MartinKun/ParkingProject",
+        path: null
+      },
+      {
+        idWork: 2,
+        titleEn: "Qultur Android",
+        titleEs: "Qultur Android",
+        descriptionEn: "Android application for a tour guide that shows tourists different points of interest in the city they are visiting, offering them a wide variety of proposals in an easy and fast way. This application is private.",
+        descriptionEs: "Aplicación de guía de turismo para android que muestra al turista distintos puntos de interés conectados en la ciudad que está visitando, ofreciéndole una amplia variedad de propuestas de una manera fácil y rápida. La aplicación es privada.",
+        image: "https://i.im.ge/2022/08/01/FPbTuh.qultur-mobile.jpg",
+        techs: ["Java 11", "Spring", "MySQL", "Android Studio"],
+        github: null,
+        path: null
+      },
+      {
+        idWork: 3,
+        titleEn: "Qultur Web",
+        titleEs: "	Qultur Web",
+        descriptionEn: "Web version of the Qultur Android App. (In process).",
+        descriptionEs: "Versión Web de la App Qultur para Android. (En proceso).",
+        image: "https://i.im.ge/2022/08/01/FPbX8Y.qultur-page.jpg",
+        techs: ["React", "Google Maps Api"],
+        github: null,
+        path: null
+      }
+
+    ],
   });
-
-  const handleClick = () => {
-    setCallBacks((prevState) => ({ ...prevState, loadingMoreWorks: true }));
-    getFeaturedWorks(works.page)
-      .then((response) => {
-        if (
-          response.data.length + works.array.length ===
-          works.limitItems + 4
-        ) {
-          setWorks((prevState) => ({
-            array: [...prevState.array, ...response.data],
-            page: works.page + 1,
-            limitItems: works.limitItems + 4,
-          }));
-          setIsShowMore(true);
-          setCallBacks((prevState) => ({
-            ...prevState,
-            loadingMoreWorks: false,
-          }));
-        } else {
-          setWorks((prevState) => ({
-            array: [...prevState.array, ...response.data],
-            page: works.page + 1,
-            limitItems: works.limitItems + 4,
-          }));
-          setIsShowMore(false);
-          setCallBacks((prevState) => ({
-            ...prevState,
-            loadingMoreWorks: false,
-          }));
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        setCallBacks((prevState) => ({
-          ...prevState,
-          loadingMoreWorks: false,
-          error: true,
-        }));
-      });
-  };
-
-  useEffect(() => {
-    setCallBacks((prevState) => ({ ...prevState, loadingFirstWorks: true }));
-    getFeaturedWorks(works.page)
-      .then((response) => {
-        if (response.data.length === works.limitItems + 4) {
-          setWorks((prevState) => ({
-            ...prevState,
-            array: response.data,
-            page: works.page + 1,
-            limitItems: works.limitItems + 4,
-          }));
-          setIsShowMore(true);
-          setCallBacks((prevState) => ({
-            ...prevState,
-            loadingFirstWorks: false,
-          }));
-        } else {
-          setWorks((prevState) => ({
-            ...prevState,
-            array: response.data,
-            page: works.page + 1,
-            limitItems: works.limitItems + 4,
-          }));
-          setIsShowMore(false);
-          setCallBacks((prevState) => ({
-            ...prevState,
-            loadingFirstWorks: false,
-          }));
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        setCallBacks((prevState) => ({
-          ...prevState,
-          loadingFirstWorks: false,
-          error: true,
-        }));
-      });
-  }, []);
 
   return (
     <div className={styles.projects}>
@@ -112,49 +62,16 @@ const Projects = () => {
       <Container>
         {reveal.works && (
           <div className={styles.works}>
-            {callbacks.loadingFirstWorks && (
-              <div style={{ margin: "auto" }}>
-                <Loader type={"page"} />
-              </div>
-            )}
             {works.array.map((project, index) => (
 
               works.limitItems === works.array.length && index + 1 === works.limitItems ? <></> :
                 <ProjectCard
-                project={project}
-                orientation={(index + 1) % 2 === 0 ? "left" : "right"}
-                key={project.id}
-              />
-              
-              
+                  project={project}
+                  orientation={(index + 1) % 2 === 0 ? "left" : "right"}
+                  key={project.id}
+                />
             ))}
 
-            {isShowMore && (
-              <button
-                className={
-                  isLightOn
-                    ? styles.showMoreBtnLightMode
-                    : styles.showMoreBtnDarkMode
-                }
-                onClick={handleClick}
-              >
-                {callbacks.loadingMoreWorks && (
-                  <span
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      margin: "0 auto",
-                    }}
-                  >
-                    <Loader type={"button"} />
-                    {language.english ? "Loading..." : "Cargando..."}
-                  </span>
-                )}
-                {!callbacks.loadingMoreWorks &&
-                  (language.english ? "Show More" : "Mostrar Más")}
-              </button>
-            )}
-            {callbacks.error && <ErrorMessage />}
           </div>
         )}
       </Container>
